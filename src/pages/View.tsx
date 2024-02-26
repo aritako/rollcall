fb11dc5a186ff7b0325348ae54605e1165f9a374
Carmelo Atienza <atienzabusiness@gmail.com>
2024-02-27 00:10:35 +0800

    feat: add sample card in dashboard view

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -1,5 +1,19 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+
+function Example() {
+    return (
+        <IonCard>
+          <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+          <IonCardHeader>
+            <IonCardTitle>Card Title</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          </IonCardHeader>
+    
+          <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+        </IonCard>
+      );
+  }
 
 const Tab1: React.FC = () => {
 
@@ -14,7 +28,7 @@ const Tab1: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding">
-                Insert Dashboard UI Here...
+                <Example />
             </IonContent>
         </IonPage>
     );
