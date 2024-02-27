c167b8f9da1415b2c1290be1f26047ff4d5c3087
Carmelo Atienza <atienzabusiness@gmail.com>
2024-02-27 21:13:41 +0800

    chore: disable unit testing for now

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:8100/');
+// test('has title', async ({ page }) => {
+//   await page.goto('http://localhost:8100/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/RollCall/);
-});
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/RollCall/);
+// });
 
 // test('get started link', async ({ page }) => {
 //   await page.goto('https://playwright.dev/');
