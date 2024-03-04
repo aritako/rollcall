3196dc7c28ed91067de20bb200a25cf6431ce1ec
Carmelo Atienza <atienzabusiness@gmail.com>
2024-03-04 12:39:27 +0800

    test: tweak configs for github-actions compatibility

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 /*
 test('has title', async ({ page }) => {
-  // await page.goto('http://localhost:8100/');
+  // await page.goto('/');
   await page.goto('https://playwright.dev/');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
@@ -18,14 +18,14 @@ test('has title', async ({ page }) => {
 //   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 // });
 
-test('HAS TITLE', async ({ page }) => {
-  await page.goto('http://localhost:8100/');
+test('Check title of application', async ({ page }) => {
+  await page.goto('/');
   await expect(page).toHaveTitle(/RollCall/);
 });
 
 
-test('Dashboard Classes', async ({ page }) => {
-  await page.goto('http://localhost:8100/app/dashboard/view');
+test('View Dashboard Classes', async ({ page }) => {
+  await page.goto('/app/dashboard/view');
   const cs192CardTitle = await page.textContent('ion-card:nth-child(2) ion-card-title');
   expect(cs192CardTitle).toContain('CS 192');
 
@@ -40,7 +40,7 @@ test('Dashboard Classes', async ({ page }) => {
 });
 
 test('Navigate to Scan tab', async ({ page }) => {
-  await page.goto('http://localhost:8100/app/dashboard/view');
+  await page.goto('/app/dashboard/view');
 
   const scanTabButton = await page.$('ion-tab-button[tab="tab2"]');
   await scanTabButton!.click();
@@ -49,7 +49,7 @@ test('Navigate to Scan tab', async ({ page }) => {
 });
 
 test('Navigate to Profile tab', async ({ page }) => {
-  await page.goto('http://localhost:8100/app/dashboard/view');
+  await page.goto('/app/dashboard/view');
 
   const scanTabButton = await page.$('ion-tab-button[tab="tab3"]');
   await scanTabButton!.click();
