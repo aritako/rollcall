12dbdf62b35db5060de3dd3ee2aa7e94f6b6d258
Carmelo Atienza <atienzabusiness@gmail.com>
2024-03-12 00:49:10 +0800

    fix: abandon environment variables

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl : string | undefined = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey : string | undefined = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl : string | undefined = "https://rckwuovuvxpzfjgzkhdq.supabase.co"
+const supabaseKey : string | undefined = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJja3d1b3Z1dnhwemZqZ3praGRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg2NTM2NzgsImV4cCI6MjAyNDIyOTY3OH0.CfJK2zFd2Kw_Lcx57AD1uf7QX502HOkz2vlngcCu7Pk"
 const supabase = createClient(supabaseUrl!, supabaseKey!)
 
 export default supabase
