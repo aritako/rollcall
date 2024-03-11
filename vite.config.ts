0ea4c82a34a03ef134e1b277e0d7810f3545a053
Carmelo Atienza <atienzabusiness@gmail.com>
2024-03-12 03:46:58 +0800

    fix!: tweak chunkSizeWarningLimit for run tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +12,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
+  },
+  // WORKAROUND ONLY!!
+  build: {chunkSizeWarningLimit: 1600}
 })
