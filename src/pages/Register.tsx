f275b6173af824bf665914341d586136074aea2a
Carmelo Atienza <atienzabusiness@gmail.com>
2024-03-13 09:33:35 +0800

    refactor: utilize supabase import

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -18,9 +18,7 @@ import {
     IonRow
 } from '@ionic/react';
 import React, {useEffect, useState} from 'react';
-import { createClient } from "@supabase/supabase-js";
-const supabase = createClient("https://rckwuovuvxpzfjgzkhdq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJja3d1b3Z1dnhwemZqZ3praGRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg2NTM2NzgsImV4cCI6MjAyNDIyOTY3OH0.CfJK2zFd2Kw_Lcx57AD1uf7QX502HOkz2vlngcCu7Pk");
-
+import supabase from "../config/supabaseClient"
 
 const Register: React.FC = () => {
     const router = useIonRouter();
