af83689abf7b1a9a048bc2cdff78ee7081eb5a06
astorres1 <astorres1@up.edu.ph>
2024-04-18 23:05:47 +0800

    test: fixed cases

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -21,7 +21,9 @@ test('Unsuccessful signup: unavailable UP Email', async ({ page }) => {
   await page.getByRole('button', { name: 'Next' }).nth(1).click();
   await page.getByRole('button', { name: 'Finish' }).click();
   await page.getByRole('link', { name: 'Create Account' }).click();
-  await page.getByLabel('Student NumberStudent Number').fill('202107015');
+  await page.getByText('Student/Professor').click();
+  await page.getByRole('button', { name: 'Student' }).click();
+  await page.getByLabel('UP ID NumberUP ID Number').fill('202107015');
   await page.getByLabel('First NameFirst Name').click();
   await page.getByLabel('First NameFirst Name').fill('Antonio');
   await page.getByLabel('Last NameLast Name').click();
@@ -44,7 +46,9 @@ test('Successful signup', async ({ page }) => {
   await page.getByRole('button', { name: 'Next' }).nth(1).click();
   await page.getByRole('button', { name: 'Finish' }).click();
   await page.getByRole('link', { name: 'Create Account' }).click();
-  await page.getByLabel('Student NumberStudent Number').fill('202107015');
+  await page.getByText('Student/Professor').click();
+  await page.getByRole('button', { name: 'Student' }).click();
+  await page.getByLabel('UP ID NumberUP ID Number').fill('202107015');
   await page.getByLabel('First NameFirst Name').click();
   await page.getByLabel('First NameFirst Name').fill('Antonio');
   await page.getByLabel('Last NameLast Name').click();
